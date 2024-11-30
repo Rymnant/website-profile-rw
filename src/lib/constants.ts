b895c6c176142d3d4af9060ce444db8011ce4b40
Rachman <159479173+Rymnant@users.noreply.github.com>
2024-12-01 03:22:57 +0700

    Deployment test

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -30,7 +30,7 @@ export const HEADER_NAV_ITEMS: HeaderNavItem[] = [
 
 // Define the hero section props
 export const HERO_PROPS = [{
-    title: 'Selamat Datang',
+    title: 'Hello World',
     subtitle: 'Di website resmi RW06 Rejowinangun',
     tagline: 'Website ini berisi informasi seputar RW06 Rejowinangun',
     buttonText: 'Tentang Kami',
